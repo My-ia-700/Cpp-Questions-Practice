95482dcfdeaae0bae5fb75875bf9a7eecf7aeaa2
My-ia-700 <bhaaw60021072@gmail.com>
2025-09-13 09:54:35 +0530

    practice

---
+++ FUNCTIONS/areaOfCircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+
+void area(int r){
+    int areaOfCircle = (3.14)*r*r ;
+    cout << " The area of circle is " << areaOfCircle << endl ;
+}
+int main (){
+    area(5);
+    return 0;
+}
