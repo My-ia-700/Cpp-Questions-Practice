525e222f2fe007d6e144a61f386026d39dd601b8
My-ia-700 <bhaaw60021072@gmail.com>
2025-09-20 12:26:53 +0530

    binary to decimal conversion

--- BITWISE OPERATER/Binary _to_Decimal.cpp
+++ BITWISE OPERATER/Binary _to_Decimal.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-// reverse
-// int reverse_digit(int num)
-// {
-//     int bit, rev = 0;
-//     while (num > 0)
-//     {
-//         bit = num % 10;
-//         rev = rev * 10 + bit;
-//         num /= 10;
-//     }
-//     return rev;
-// }
-// stright
+// method 1
 // 101 = 2^2 * 1 + 2^1 * 0 +2^0 * 1 = 5
-int binary_to_deca(int num)
+int binaryTodecimal(int num)
 {
-    int decimal = 0, base = 1, rem ;
+    int decimal = 0, power = 0;
     while (num > 0)
     {
-        rem = num % 10;
-        decimal += rem * base;
-        base *= 2;
-        num /= 10;
+        int last_bit = num % 10;                      // 101%10=1 , 10%10=0 , 1%10=1
+        decimal = decimal + pow(2, power) * last_bit; // 0+2^0*1=1 , 1+2^1*0=1 , 1+2^2*1=5
+        power++;                                      // 0->1, 1->2 , 2->3
+        num /= 10;                                    // 101/10=10 , 10/10=1 , 1/10=0
     }
     return decimal;
 }
 int main()
 {
     int n;
-    cout << "Enter a binary no. : ";
+    cout << " Enter a Binary Number : ";
     cin >> n;
-    // int bits = reverse_digit(n);
-    int binaryTodeca = binary_to_deca(n);
-    cout << "The Decimal representation of " << n << " is " << binaryTodeca;
+    int binary_to_deciaml = binaryTodecimal(n);
+    cout << " The Decimal repreatation of " << n << " is " << binary_to_deciaml;
+    return 0;
 }
+// method 2
+// int binary_to_deca(int num)
+// {
+//     int decimal = 0, base = 1, rem;
+//     while (num > 0)
+//     {
+//         rem = num % 10;        // 101 % 10 = 1 , 10%10=0 , 1%10=1
+//         decimal += rem * base; // 0+1*1 = 1 , 1+0*2=1 , {1+1*4=5}// print hoga
+//         base *= 2;             // 1*2=2 , 2*2=4 , 4*2=8
+//         num /= 10;             // 101/10=10,10/10=1 , 1/10=0
+//     }
+//     return decimal;
+// }
+// int main()
+// {
+//     int n;
+//     cout << "Enter a binary no. : ";
+//     cin >> n;
+//     int binaryTodeca = binary_to_deca(n);
+//     cout << "The Decimal representation of " << n << " is " << binaryTodeca;
+// }
