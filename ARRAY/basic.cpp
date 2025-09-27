6d6fdbbcd94efcf63c7b5a9620733c45571f96d9
My-ia-700 <bhaaw60021072@gmail.com>
2025-09-27 16:45:10 +0530

    add files

---
+++ ARRAY/basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main()
+{ 
+    // array creation
+    int arr[456];
+    float num[52];
+    char ch[26];
+    long lnum[2535];
+    short snum[1000];
+
+    cout << "Array creation successfully"<< endl ;
+    return 0;
+}
--- BITWISE OPERATER/Octal_to_binary.cpp
+++ BITWISE OPERATER/Octal_to_binary.cpp
@@ -1,3 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Function to convert a single octal digit to 3-bit binary and append to result
+int appendBinary(int result, int octalDigit, int &place) {
+    int bin = 0;
+    int binPlace = 1;
+    // Convert octal digit to binary (3 bits)
+    for (int i = 0; i < 3; i++) {
+        bin += (octalDigit % 2) * binPlace;
+        octalDigit /= 2;
+        binPlace *= 10;
+    }
+    // Append binary to the left of result
+    result += bin * place;
+    place *= 1000; // Move place by 3 digits for next binary
+    return result;
+}
+
+int octalToBinary(int octal) {
+    int result = 0;
+    int place = 1;
+    while (octal > 0) {
+        int digit = octal % 10;
+        result = appendBinary(result, digit, place);
+        octal /= 10;
+    }
+    return result;
+}
+
+int main() {
+    int octal;
+    cout << "Enter octal number: ";
+    cin >> octal;
+    int binary = octalToBinary(octal);
+    cout << "Binary number: " << binary << endl;
+    return 0;
+}
 // #include <iostream>
 // #include <cmath>
 // using namespace std;
@@ -35,48 +73,48 @@
 //     cout << "The Binary representation of Octal Number " << n << " is " << reverse;
 //     return 0;
 // }
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-// reverse the digit
-int reverseDigit(int num)
-{
-    int digit, rev = 0;
-    while (num > 0)
-    {
-        digit = num % 10;       //
-        rev = rev * 10 + digit; //
-        num /= 10;              //
-    }
-    return rev;
-}
+// // reverse the digit
+// int reverseDigit(int num)
+// {
+//     int digit, rev = 0;
+//     while (num > 0)
+//     {
+//         digit = num % 10;       //
+//         rev = rev * 10 + digit; //
+//         num /= 10;              //
+//     }
+//     return rev;
+// }
 
-// print the reversed number
-int printDigit(int num) // num = rev = 457
-{
-    int rev = 0;
-    while (num > 0)
-    {
-        int digit = num % 10; //
-        int bit = 0; 
-        int place = 1;
-        while (digit > 0)
-        {
-            bit = bit + (digit%2)*place; 
-            digit /= 2 ; // 
-            place *= 10 ; //
-        }
-        rev = rev * 1000 + bit; // 
-        num /= 10; //
-    }
-    return rev;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    int rev = reverseDigit(n); // rev = 
-    int binary = printDigit(rev);
-    cout << binary;
-    return 0;
-}
+// // print the reversed number
+// int printDigit(int num) // num = rev = 457
+// {
+//     int rev = 0;
+//     while (num > 0)
+//     {
+//         int digit = num % 10; //
+//         int bit = 0; 
+//         int place = 1;
+//         while (digit > 0)
+//         {
+//             bit = bit + (digit%2)*place; 
+//             digit /= 2 ; // 
+//             place *= 10 ; //
+//         }
+//         rev = rev * 1000 + bit; // 
+//         num /= 10; //
+//     }
+//     return rev;
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int rev = reverseDigit(n); // rev = 
+//     int binary = printDigit(rev);
+//     cout << binary;
+//     return 0;
+// }
