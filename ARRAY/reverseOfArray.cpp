8b05b3f3191543efbf9141f4b9f85b6598a0f9ac
My-ia-700 <bhaaw60021072@gmail.com>
2025-11-11 13:50:45 +0530

    add some practice files

---
+++ ARRAY/reverseOfArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// arr = {1,2,3,4,5} -> {5,4,3,2,1}
+void reverseArray(int arr[], int size)
+{
+    int left = 0;
+    int right = size - 1;
+    // for(int left = 0 ,right = size - 1 ; left<=right ; left++,right-- ){
+    //     swap(arr[left],arr[right])
+    // }
+    while (left <= right)
+    { // condition for both even and odd no. of array
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[7] = {1, 2, 3, 4, 5};
+    int size = 5;
+    reverseArray(arr, size);
+    return 0;
+}
---
+++ CLASS/class1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " Hello World " << endl ;
+    int num1 = 5, num2 = 6;
+    int sum = num1 + num2;
+    cout << " The sum is : " << sum << endl;
+    return 0;
+}
